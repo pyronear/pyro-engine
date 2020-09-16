68cbb5975283a76ac40d5871ebde0ce9450792d1
lostanlen <mateo.lostanlen@gmail.com>
2020-09-16 19:47:00 +0200

    Setup (#1)
    * add test workflow
    
    * add missing file (doc)
    
    * add test folder
    
    * remove pycache files
    
    * remove test utils from older repo
    
    * remove test folder
    
    * add setup
    
    * add test test
    
    * move coverage
    
    * move coverage 2
    
    * fix setup
    
    * fix setup 2
    
    * fix setup 3
    
    * remove pkg test
    
    * put back pkg test
    
    * fix setup 4
    
    * add ci providers
    
    * update requirement
    
    * remove ci providers
    
    * remove useless files
    
    * remove model checkpoint
    
    * fix codcove version
    
    * remove pycache files
    
    * allow dynamic checkpoint path
    
    * remove binary files
    
    * add gitignore
    
    * remove tests jobs

---
+++ setup.py
@@ -0,0 +1,83 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) Pyronear contributors.
+# This file is dual licensed under the terms of the CeCILL-2.1 and AGPLv3 licenses.
+# See the LICENSE file in the root of this repository for complete details.
+
+"""
+Package installation setup
+"""
+
+import os
+import subprocess
+from setuptools import setup, find_packages
+
+
+package_name = 'pyronearEngine'
+with open(os.path.join('pyronearEngine', 'version.py')) as version_file:
+    version = version_file.read().strip()
+sha = 'Unknown'
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=cwd).decode('ascii').strip()
+except Exception:
+    pass
+
+if os.getenv('BUILD_VERSION'):
+    version = os.getenv('BUILD_VERSION')
+elif sha != 'Unknown':
+    version += '+' + sha[:7]
+print("Building wheel {}-{}".format(package_name, version))
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    # Metadata
+    name=package_name,
+    version=version,
+    author='PyroNear Contributors',
+    author_email='pyronear.d4g@gmail.com',
+    maintainer='Pyronear',
+    description='Pyronear Engine is a repository that aims at deploying pyronear',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url='https://github.com/pyronear//PyronearEngine',
+    download_url='https://github.com/pyronear/PyronearEngine/tags',
+    license='CeCILL-2.1 or AGPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
+        "License :: OSI Approved :: GNU Affero General Public License v3 (AGPLv3)",
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=['pytorch', 'deep learning', 'vision', 'models',
+              'wildfire', 'object detection'],
+
+    # Package info
+    packages=find_packages(exclude=('test',)),
+    zip_safe=True,
+    python_requires='>=3.6.0',
+    include_package_data=True,
+    install_requires=requirements,
+    package_data={'': ['LICENSE']}
+)
