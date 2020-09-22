ae8bcf9e32053fbe7f6b437e9eb1a9db9f5cb989
lostanlen <mateo.lostanlen@gmail.com>
2020-09-22 10:22:39 +0200

    Webapp (#3)
    * add test workflow
    
    * add missing file (doc)
    
    * add test folder
    
    * remove pycache files
    
    * remove test utils from older repo
    
    * remove test folder
    
    * add setup
    
    * add test test
    
    * move coverage
    
    * move coverage 2
    
    * fix setup
    
    * fix setup 2
    
    * fix setup 3
    
    * remove pkg test
    
    * put back pkg test
    
    * fix setup 4
    
    * add ci providers
    
    * update requirement
    
    * remove ci providers
    
    * remove useless files
    
    * remove model checkpoint
    
    * fix codcove version
    
    * remove pycache files
    
    * allow dynamic checkpoint path
    
    * remove binary files
    
    * add gitignore
    
    * remove tests jobs
    
    * add web app
    
    * ignore checkpoints
    
    * update init file
    
    * limit the imported objects

---
+++ pyronearEngine/webApp/app/app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+from pyronearEngine.inference.pyronearPredict import PyronearPredictor
+
+pyronearPredictor = PyronearPredictor("model/pyronearModel.pth")
+
+st.image(Image.open('logo//pyronear-logo-dark.png'), use_column_width=True)
+st.title("PyroNear: wildfire early detection")
+st.header('The Pyronear detector has been trained to detect a fire outbreak in a forest, \
+           please use it only in this context')
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Analyzed image', use_column_width=True)
+
+    res = pyronearPredictor.predict(image)
+
+    if res[0] > 0.5:
+        st.write('No fire detected :evergreen_tree:')
+
+    else:
+        st.write('Fire detected :fire:')
