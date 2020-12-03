ae78e97378347d43b2dee95bf6eaf489151b2550
matéo lostanlen <mateo.lostanlen@gmail.com>
2020-12-03 14:48:51 +0100

    update config

--- pyroengine/inference/configManager.py
+++ pyroengine/inference/configManager.py
@@ -1,7 +1,7 @@
 import configparser
 
 
-def readConfigFile(filename):
+def read_config_file(filename):
     """Read the config file and return it as a dictionary."""
     config = configparser.ConfigParser()
     config.read(filename)
@@ -13,20 +13,20 @@ def readConfigFile(filename):
     return conf
 
 
-def writeConfigFile(filename='inference.cfg'):
+def write_config_file(filename='inference.cfg'):
     """Use config parser to save config file."""
 
     config = configparser.ConfigParser()
     config['MODEL'] = {'backbone': 'rexnet1_0x',
                        'num_classes': 1,
-                       'use_AdaptiveConcatPool2d_head': True,
+                       'use_adaptive_concat_pool2d_head': True,
                        'nb_features': 1280,
                        'cut': -2,
                        'checkpoint': 'pyro_checkpoint_V0.1.pth',
                        'device': 'cuda'}
 
-    config['TRANSFORMS'] = {'imageSize': 448,
-                            'use_CenterCrop': True}
+    config['TRANSFORMS'] = {'image_size': 448,
+                            'use_center_crop': True}
 
     with open(filename, 'w') as configfile:
         config.write(configfile)
@@ -35,4 +35,4 @@ def writeConfigFile(filename='inference.cfg'):
 if __name__ == '__main__':
 
     # Run this module to save your config
-    writeConfigFile()
+    write_config_file()
