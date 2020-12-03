e9b8431b5a050012475b3d8cc7f1e7a9de150d5a
matéo lostanlen <mateo.lostanlen@gmail.com>
2020-12-03 17:53:37 +0100

    update pyroEngine for pc

--- pyroengine/pc/pyronearEngine.py
+++ pyroengine/pc/pyronearEngine.py
@@ -5,55 +5,64 @@
 # See the LICENSE file in the root of this repository for complete details.
 
 import cv2
-from time import sleep
-import glob
 from PIL import Image
-import smtplib
-import ssl
+import numpy as np
+from pyroengine.inference.pyronearPredict import PyronearPredictor
 
 
 class PyronearEngine:
-    """This class is the Pyronear Engine. This engine manage the whole Fire Detection
-       process by capturing and saving the image and by predicting if there is a fire or
-       not based on this image.
+    """The Pyronear Engine manage the whole Fire Detection process by capturing and saving the image,
+       but also by predicting if there is a fire or not based on this image.
+
     Example
     -------
-    pyronearEngine = PyronearEngine("model/pyronearModel.pth")
-    pyronearEngine.run(30)  # For a prediction every 30s
+    pyroEngine = PyronearEngine(configFile, checkpointFile)
+    pyroEngine.run_video()
+
     """
-    def __init__(self, imgsFolder, checkpointPath):
-        # Camera
-        self.camera = cv2.VideoCapture(0)
 
+    def __init__(self, config, checkpoint):
+        """Init Pyronear Engine."""
         # Pyronear Predictor
-        self.pyronearPredictor = PyronearPredictor(checkpointPath)
+        self.pyronearPredictor = PyronearPredictor(config, checkpoint)
 
-    def run_video(self, timeStep):
+    def run_video(self, video=None, logo=None):
+        """
+        Perform inference on video file.
 
-        cap = cv2.VideoCapture(0)
+        Args:
+        ----
+            video (str, optional): Path to a video. Use pc camera by default.
+            logo (str, optional): Path to pyronear logo in imgs folder.
+
+        """
+        if logo:
+            logo = cv2.imread(str)
+            logo = cv2.resize(logo, (150, 30))
+        else:
+            logo = 255 * np.ones((30, 150, 3))
+            logo = cv2.putText(logo, str('PYRONEAR'), (5, 22), 0, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        if video:
+            cap = cv2.VideoCapture(video)
+        else:
+            cap = cv2.VideoCapture(0)
         frameNb = 0
         while(True):
             # Capture frame-by-frame
             _, frame = cap.read()
-            frameNb+=1
-            
-            if frame.any():
-            
-                #frame = np.transpose(frame, (1, 0, 2))
-                im = Image.fromarray(frame[:,:,::-1])
-                
-                res = pred(im)
+            frameNb += 1
 
-                res = (res*1000)//1/1000
-            
-                if ii%3 == 0:
+            if frame.any():  
+                im = Image.fromarray(frame[:, :, ::-1])
+                res = self.pyronearPredictor.predict(im)
 
-                    frame[:logo.shape[0],:logo.shape[1],:]=logo
-                    
+                if frameNb % 3 == 0:
+                    frame[:logo.shape[0], :logo.shape[1], :] = logo
+
                     if res < 0.5:
-                        cv2.putText(frame, str(res), (95, 22), 0, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+                        cv2.putText(frame, "%.3f" % res, (95, 22), 0, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
                     else:
-                        cv2.putText(frame, str(res), (95, 22), 0, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                        cv2.putText(frame, "%.3f" % res, (95, 22), 0, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                         cv2.imwrite('fire.jpg', frame)
                         # Send alert to api
                         self.call_api(res)
@@ -74,18 +83,14 @@ class PyronearEngine:
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
-    def run(self, imagePath):
-
-        self.camera.start_preview()
-        sleep(3)  # gives the camera’s sensor time to sense the light levels
-        self.camera.capture(imagePath)
-        self.camera.stop_preview()
-
     def call_api(self, score):
+        """Send alert to the api."""
         pass
 
 
 if __name__ == "__main__":
 
-    pyronearEngine = PyronearEngine('DS', "model/pyronearModel.pth")
-    pyronearEngine.run(5)  # For a prediction every 5s
+    checkpoint = "pyro_checkpoint_V0.1.pth"
+    config = "inference.cfg"
+    pyroEngine = PyronearEngine(config, checkpoint)
+    pyroEngine.run_video()
