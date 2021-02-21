26126805232619e25bfc9a38588efa2ca205ff47
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2021-02-21 19:07:28 +0100

    update & rename pyronear predictor

--- pyroengine/engine/pyronear_predictor.py
+++ pyroengine/engine/pyronear_predictor.py
@@ -3,45 +3,38 @@
 # This program is licensed under the GNU Affero General Public License version 3.
 # See LICENSE or go to <https://www.gnu.org/licenses/agpl-3.0.txt> for full license details.
 
-
-import torchvision
-import torch
-from torch import nn
-from PIL import Image
+from pyrovision.models.rexnet import rexnet1_0x
 from torchvision import transforms
+from PIL import Image
+import torch
 
 
 class PyronearPredictor:
     """This class use the last pyronear model to predict if a sigle frame contain a fire or not
     Example
     -------
-    pyronearPredictor = PyronearPredictor("model/pyronear.pth")
+    pyronearPredictor = PyronearPredictor()
+    im = Image.open("image.jpg")
     res = pyronearPredictor.predict(im)
-    print(res) #res[0,1]=fire probability
     """
-    def __init__(self, checkpointPath):
+    def __init__(self):
         # Model definition
-        self.model = torchvision.models.resnet18()
-
-        # Change fc
-        in_features = getattr(self.model, 'fc').in_features
-        setattr(self.model, 'fc', nn.Linear(in_features, 2))
-
-        self.model.load_state_dict(torch.load(checkpointPath))
+        self.model = rexnet1_0x(pretrained=True)
+        self.model.eval()
 
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        img_size = 448
 
-        self.tf = transforms.Compose([transforms.Resize((224, 224)),
+        self.tf = transforms.Compose([transforms.Resize(size=(img_size)),
+                                      transforms.CenterCrop(size=img_size),
                                       transforms.ToTensor(),
-                                      normalize])
-
-        self.softmax = nn.Softmax(dim=1)
+                                      normalize
+                                      ])
 
     def predict(self, im):
         imT = self.tf(im)
 
-        self.model.eval()
         with torch.no_grad():
             pred = self.model(imT.unsqueeze(0))
 
-        return self.softmax(pred).squeeze().numpy()
+        return torch.sigmoid(pred).item()
