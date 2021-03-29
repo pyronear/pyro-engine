066f110792324125293ab49d26d41ccd409df4dd
fe <veith.felix@gmail.com>
2021-03-29 19:30:24 +0200

    style: fix lint

--- server/src/tests/routes/test_inference.py
+++ server/src/tests/routes/test_inference.py
@@ -2,11 +2,11 @@ import json
 import requests
 import os
 
+
 def test_inference(test_app):
 
     img_content = requests.get("https://pyronear.org/img/logo_letters.png").content
 
-    response = test_app.post("/inference/",
-    						files = dict(file=img_content))
+    response = test_app.post("/inference/", files=dict(file=img_content))
 
     assert response.status_code == 201
