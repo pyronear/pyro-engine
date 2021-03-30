00d1ea1e10d5dde784892350f1b28a09ef9197b4
fe <veith.felix@gmail.com>
2021-03-30 16:25:04 +0200

    refactor: remove useless config elts

--- server/src/app/config.py
+++ server/src/app/config.py
@@ -10,6 +10,4 @@ PROJECT_NAME: str = 'Pyronear local system API'
 PROJECT_DESCRIPTION: str = 'API for pyronear systems interactions on sites'
 API_BASE: str = 'api/'
 VERSION: str = "0.1.0a0"
-DEBUG: bool = os.environ.get('DJANGO_DEBUG', '') != 'False'
-DATABASE_URL: str = os.getenv("DATABASE_URL")
 LOGO_URL: str = "https://github.com/pyronear/PyroNear/raw/master/docs/source/_static/img/pyronear-logo-dark.png"
