09d387bdd852da2391d267fb1881db16903047cd
lostanlen <mateo.lostanlen@gmail.com>
2021-04-09 18:33:13 +0200

    Test patch (#16)
    * fix import
    
    * create fake lib for testing
    
    * fix reponame
    
    * update requirements
    
    * fix indent

---
+++ test/pi_patch.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2021, Pyronear contributors.
+
+# This program is licensed under the GNU Affero General Public License version 3.
+# See LICENSE or go to <https://www.gnu.org/licenses/agpl-3.0.txt> for full license details.
+
+import sys
+import fake_gpiozero
+
+
+sys.modules['gpiozero'] = fake_gpiozero 
