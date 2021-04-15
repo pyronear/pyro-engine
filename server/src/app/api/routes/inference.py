faa38fc129a5c6b75b6d102e7ecd142ad5bf5dcb
lostanlen <mateo.lostanlen@gmail.com>
2021-04-15 15:29:32 +0200

    Inference using engine (#20)
    * add engine in api
    
    * add pizero capture script
    
    * fix prediction inside docker
    
    * update compose
    
    * final runner
    
    * fix flake 8
    
    * fix ports
    
    * blank line

--- server/src/app/api/routes/inference.py
+++ server/src/app/api/routes/inference.py
@@ -6,27 +6,31 @@
 
 from fastapi import APIRouter, BackgroundTasks
 from fastapi import File, UploadFile
+from pyroengine.engine import PyronearEngine
+from PIL import Image
+import io
+
 
 router = APIRouter()
+engine = PyronearEngine()  # need to add api setup parameters here
 
 
 def predict_and_alert(file):
     """ predict smoke from an image and if yes raise an alert """
-    # TODO : use inference class and pyro-api client to raise an alert if needed
-    return {"comment": "set fire to the rain please"}
+    # Load Image
+    image = Image.open(io.BytesIO(file))
+
+    # Predict
+    engine.predict(image)
 
 
-@router.post("/", status_code=201, summary="Send img from a device to predict smoke")
+@router.post("/file/", status_code=201, summary="Send img from a device to predict smoke")
 async def inference(background_tasks: BackgroundTasks,
                     file: UploadFile = File(...)
                     ):
     """
-    Send img from a device based on the given information in order to predict smoke
-
-    Below, click on "Schema" for more detailed information about arguments
-    or "Example Value" to get a concrete idea of arguments
+    Get image from pizero and call engine for wildfire detection
     """
-    # some async operation could happen here
 
-    # We might need a backround task here for inference
-    background_tasks.add_task(predict_and_alert, file)
+    # Call engine as background task
+    background_tasks.add_task(predict_and_alert, await file.read())
