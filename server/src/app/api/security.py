d5c96a080fe546431cfd27415eba645810eea23f
fe <veith.felix@gmail.com>
2021-04-17 20:29:21 +0200

    feat: add security fonctions

--- server/src/app/api/security.py
+++ server/src/app/api/security.py
@@ -1,14 +1,10 @@
-from jose import JWTError, jwt
+from jose import jwt
 from passlib.context import CryptContext
 from typing import Optional
 from datetime import datetime, timedelta
 
+from app import config as cfg
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -23,12 +19,14 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token(data: dict, expires_delta: timedelta):
+
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    expire = datetime.utcnow() + expires_delta
+
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    encoded_jwt = jwt.encode(to_encode, cfg.SECRET_KEY, algorithm=cfg.JWT_ENCODING_ALGORITHM)
+
     return encoded_jwt
