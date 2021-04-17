33cb64ac05867cedac4cdd139c61b246b6b0a384
fe <veith.felix@gmail.com>
2021-04-17 20:30:41 +0200

    feat: add auth to inference route

--- server/src/app/api/routes/inference.py
+++ server/src/app/api/routes/inference.py
@@ -4,8 +4,11 @@
 # See LICENSE or go to <https://www.gnu.org/licenses/agpl-3.0.txt> for full license details.
 
 
-from fastapi import APIRouter, BackgroundTasks
+from fastapi import APIRouter, BackgroundTasks, Depends
 from fastapi import File, UploadFile
+
+from app.api.deps import get_current_active_user
+from app.api.schemas import User
 from pyroengine.engine import PyronearEngine
 from PIL import Image
 import io
@@ -26,8 +29,8 @@ def predict_and_alert(file):
 
 @router.post("/file/", status_code=201, summary="Send img from a device to predict smoke")
 async def inference(background_tasks: BackgroundTasks,
-                    file: UploadFile = File(...)
-                    ):
+                    file: UploadFile = File(...),
+                    current_user: User = Depends(get_current_active_user)):
     """
     Get image from pizero and call engine for wildfire detection
     """
