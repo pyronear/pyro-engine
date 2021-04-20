19887a50bccef4440fd3e104b691c260d27880ae
fe <veith.felix@gmail.com>
2021-04-20 12:58:55 +0200

    fix: get secret key from varenv

--- server/src/app/config.py
+++ server/src/app/config.py
@@ -3,7 +3,7 @@
 # This program is licensed under the GNU Affero General Public License version 3.
 # See LICENSE or go to <https://www.gnu.org/licenses/agpl-3.0.txt> for full license details.
 
-
+import os
 import json
 
 PROJECT_NAME: str = 'Pyronear local webserver'
@@ -17,9 +17,7 @@ LOGO_URL: str = "https://github.com/pyronear/PyroNear/raw/master/docs/source/_st
 JWT_ENCODING_ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 365 * 10
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = os.getenv("WEBSERVER_SECRET_KEY")
 
 # fleet db to be readed from env of json file outside the repo
 with open('fleet_accesses.json') as f:
