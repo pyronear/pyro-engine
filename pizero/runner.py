4ef9223c454ed6ba9c536a17e2f6e11a700ec0d1
Chloé Sekkat <57073096+chloeskt@users.noreply.github.com>
2021-04-21 13:48:44 +0200

    create env file and remove not used import (#33)

--- pizero/runner.py
+++ pizero/runner.py
@@ -5,25 +5,32 @@
 
 
 import io
+import os
 import time
 import picamera
-from PIL import Image
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+WEBSERVER_IP = os.environ.get("WEBSERVER_IP")
+WEBSERVER_PORT = os.environ.get("WEBSERVER_PORT")
 
 
-url = "http://192.168.1.62:8002/inference/file"  # api url
+url = f"http://{WEBSERVER_IP}:{WEBSERVER_PORT}/inference/file"
+
 stream = io.BytesIO()
 with picamera.PiCamera() as camera:
     camera.resolution = (3280, 2464)  # use maximal resolution
-    while(True):
-        print('capture')
+    while True:
+        print("capture")
         stream = io.BytesIO()
         camera.start_preview()
         time.sleep(3)  # small sleep here improve image quality
-        camera.capture(stream, format='jpeg')
+        camera.capture(stream, format="jpeg")
         # "Rewind" the stream to the beginning so we can read its content
         stream.seek(0)
-        files = {'file': stream}
+        files = {"file": stream}
         requests.post(url, files=files)  # send image to pi_cental
 
         time.sleep(3)  # Wait between two capture
