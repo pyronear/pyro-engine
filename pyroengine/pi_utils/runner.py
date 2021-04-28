49d9ae75c3d128770b2ee57892409606b9426132
Chloé Sekkat <57073096+chloeskt@users.noreply.github.com>
2021-04-28 20:00:33 +0200

    rewrite of runner.py to make it similar to monitor_pi.py (#36)

--- pyroengine/pi_utils/runner.py
+++ pyroengine/pi_utils/runner.py
@@ -3,13 +3,14 @@
 # This program is licensed under the GNU Affero General Public License version 3.
 # See LICENSE or go to <https://www.gnu.org/licenses/agpl-3.0.txt> for full license details.
 
-
+import logging
 import io
 import os
 import time
 import picamera
 import requests
 from dotenv import load_dotenv
+from requests import RequestException
 
 load_dotenv()
 
@@ -17,20 +18,49 @@ WEBSERVER_IP = os.environ.get("WEBSERVER_IP")
 WEBSERVER_PORT = os.environ.get("WEBSERVER_PORT")
 
 
-url = f"http://{WEBSERVER_IP}:{WEBSERVER_PORT}/inference/file"
+class Runner:
+    """This class aims at taking picture using PiCamera and sending the stream to local webserver in main raspberry"""
 
-stream = io.BytesIO()
-with picamera.PiCamera() as camera:
-    camera.resolution = (3280, 2464)  # use maximal resolution
-    while True:
-        print("capture")
+    logger = logging.getLogger(__name__)
+    CAPTURE_DELAY = 3
+    LOOP_INTERVAL = 3
+
+    def __init__(self, webserver_url):
+        """Initialize parameters for Runner."""
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (3280, 2464)  # use maximal resolution
+        self.webserver_url = webserver_url
+        self.is_running = True
+
+    def capture_stream(self):
         stream = io.BytesIO()
-        camera.start_preview()
-        time.sleep(3)  # small sleep here improve image quality
-        camera.capture(stream, format="jpeg")
+        self.camera.start_preview()
+        time.sleep(self.CAPTURE_DELAY)  # small sleep here improve image quality
+        self.camera.capture(stream, format="jpeg")
         # "Rewind" the stream to the beginning so we can read its content
         stream.seek(0)
-        files = {"file": stream}
-        requests.post(url, files=files)  # send image to pi_cental
+        return {"file": stream}
 
-        time.sleep(3)  # Wait between two capture
+    def send_stream(self, files):
+        try:
+            response = requests.post(
+                self.webserver_url, files=files
+            )  # send image to local webserver
+            response.raise_for_status()
+        except RequestException as e:
+            self.logger.error(f"Unexpected error in get_record(): {e!r}")
+
+    def run(self):
+        while self.is_running:
+            files = self.capture_stream()
+            self.send_stream(files)
+            time.sleep(self.LOOP_INTERVAL)  # Wait between two captures
+
+    def stop_runner(self):
+        self.is_running = False
+
+
+if __name__ == "__main__":
+    webserver_local_url = f"http://{WEBSERVER_IP}:{WEBSERVER_PORT}/inference/file"
+    runner = Runner(webserver_local_url)
+    runner.run()
