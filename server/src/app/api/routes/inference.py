a17ed41a838cf3894912fb66a7b70e597fec2661
MateoLOSTANLEN <mateo.lostanlen@gmail.com>
2021-06-06 07:17:57 +0200

    update inference script

--- server/src/app/api/routes/inference.py
+++ server/src/app/api/routes/inference.py
@@ -4,28 +4,58 @@
 # See LICENSE or go to <https://www.gnu.org/licenses/agpl-3.0.txt> for full license details.
 
 
-from fastapi import APIRouter, BackgroundTasks
+from fastapi import APIRouter, BackgroundTasks, Request
 from fastapi import File, UploadFile
 from pyroengine.engine import PyronearEngine
 from PIL import Image
 import io
+import json
 
 
 router = APIRouter()
-engine = PyronearEngine()  # need to add api setup parameters here
+engine, pi_zeros_data = setup_engine()
 
 
-def predict_and_alert(file):
+def setup_engine():
+    with open('config_data.json') as json_file:
+        config_data = json.load(json_file)
+
+    # Loading config datas
+    detection_threshold = config_data['detection_threshold']
+    api_url = config_data['api_url']
+    save_evry_n_frame = config_data['save_evry_n_frame']
+    latitude = config_data['latitude']
+    longitude = config_data['longitude']
+
+    # Loading pi zeros datas
+    with open('pi_zeros_data.json') as json_file:
+        pi_zeros_data = json.load(json_file)
+
+    pi_zero_credentials = {}
+    for ip in pi_zeros_data .keys():
+        d = pi_zeros_data[ip]
+        pi_zero_credentials[d['id']] = {'login': d['login'], 'password': d['password']}
+
+    engine = PyronearEngine(detection_threshold, api_url, pi_zero_credentials, save_evry_n_frame, latitude, longitude)
+
+    return engine, pi_zeros_data
+
+
+def predict_and_alert(file, ip):
     """ predict smoke from an image and if yes raise an alert """
     # Load Image
     image = Image.open(io.BytesIO(file))
 
+    # Get pi zero id
+    pi_zero_id = pi_zeros_data[ip]
+
     # Predict
-    engine.predict(image)
+    engine.predict(image, pi_zero_id)
 
 
 @router.post("/file/", status_code=201, summary="Send img from a device to predict smoke")
-async def inference(background_tasks: BackgroundTasks,
+async def inference(request: Request,
+                    background_tasks: BackgroundTasks,
                     file: UploadFile = File(...)
                     ):
     """
@@ -33,4 +63,4 @@ async def inference(background_tasks: BackgroundTasks,
     """
 
     # Call engine as background task
-    background_tasks.add_task(predict_and_alert, await file.read())
+    background_tasks.add_task(predict_and_alert, await file.read(), request.client.host)
