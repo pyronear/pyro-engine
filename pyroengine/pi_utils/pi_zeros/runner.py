f819f6dff79a55ca13c9994b19ec7ba03a4c4510
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2022-02-13 18:05:48 +0100

    add max it

--- pyroengine/pi_utils/pi_zeros/runner.py
+++ pyroengine/pi_utils/pi_zeros/runner.py
@@ -24,14 +24,15 @@ class Runner:
 
     logger = logging.getLogger(__name__)
     CAPTURE_DELAY = 3
-    LOOP_INTERVAL = 3
 
-    def __init__(self, webserver_url):
+    def __init__(self, webserver_url, loop_interval=30, max_iteration=None):
         """Initialize parameters for Runner."""
         self.camera = picamera.PiCamera()
         self.camera.resolution = (3280, 2464)  # use maximal resolution
         self.webserver_url = webserver_url
         self.is_running = True
+        self.loop_interval = loop_interval
+        self.max_iteration = max_iteration
 
     def capture_stream(self):
         stream = io.BytesIO()
@@ -52,10 +53,14 @@ class Runner:
             self.logger.error(f"Unexpected error in get_record(): {e!r}")
 
     def run(self):
+        it = 0
         while self.is_running:
             files = self.capture_stream()
             self.send_stream(files)
-            time.sleep(self.LOOP_INTERVAL)  # Wait between two captures
+            time.sleep(self.loop_interval)  # Wait between two captures
+            if self.max_iteration is not None:
+                if it > self.max_iteration:
+                    break
 
     def stop_runner(self):
         self.is_running = False
@@ -65,11 +70,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Take picture(s) and send to local web server')
     parser.add_argument('--single', action='store_true', help='Single picture instead of eternal loop')
     parser.add_argument('--write', action='store_true', help='Use /write_image route instead of /inference')
+    parser.add_argument('--loop_interval', default=30, action='store_true', help='Time between two photos')
+    parser.add_argument('--max_iteration', default=None, action='store_true', help='Time between two photos')
     args = parser.parse_args()
 
     webserver_local_url = f"http://{WEBSERVER_IP}:{WEBSERVER_PORT}" + \
                           ("/inference/file" if not args.write else "/write_image/file")
-    runner = Runner(webserver_local_url)
+                     
+    runner = Runner(webserver_local_url, loop_interval=args.loop_interval, max_iteration=args.max_iteration)
     if args.single:
         files = runner.capture_stream()
         runner.send_stream(files)
