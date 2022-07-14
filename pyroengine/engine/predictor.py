2da13c93e86c5c0d1e2331c7f404b4cf15f2e611
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2022-07-14 08:49:44 +0200

    load model from ultralytics

--- pyroengine/engine/predictor.py
+++ pyroengine/engine/predictor.py
@@ -3,39 +3,27 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-from pyrovision.models.rexnet import rexnet1_0x
-from torchvision import transforms
 import torch
 
 
 class PyronearPredictor:
     """This class use the last pyronear model to predict if a sigle frame contain a fire or not
     Examples:
-        >>> pyronearPredictor = PyronearPredictor()
+        >>> pyronearPredictor = PyronearPredictor("path/to/model.pt")
         >>> im = Image.open("image.jpg")
         >>> res = pyronearPredictor.predict(im)
     """
-    def __init__(self):
+
+    def __init__(self, path_to_model, conf=0.25):
         """Init predictor"""
         # Model definition
-        self.model = rexnet1_0x(pretrained=True).eval()
-
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        img_size = 448
-
-        self.tf = transforms.Compose([
-            transforms.Resize(size=img_size),
-            transforms.CenterCrop(size=img_size),
-            transforms.ToTensor(),
-            normalize
-        ])
+        self.model = torch.hub.load(
+            "ultralytics/yolov5", "custom", path=path_to_model
+        )  # local model
+        self.model.conf = conf
 
     def predict(self, im):
         """Run prediction"""
-        imT = self.tf(im)
+        pred = self.model(im)
 
-        with torch.no_grad():
-            pred = self.model(imT.unsqueeze(0))
-            pred = torch.sigmoid(pred).item()
-
-        return pred
+        return pred.xyxy[0].numpy()
