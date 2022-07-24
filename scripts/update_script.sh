17a4b7a4467d691fac831b9a6c5305a526de51b3
Mateo <mateo.lostanlen@gmail.com>
2022-07-24 19:31:29 +0200

    fix update script (#96)

--- scripts/update_script.sh
+++ scripts/update_script.sh
@@ -9,13 +9,13 @@
 # 0 3 * * * bash /home/pi/pyro-engine/scripts/update_script.sh
 
 
-CID=$(docker ps | grep server_web | awk '{print $1}')
+CID=$(docker ps | grep runner_pyro-engine_1 | awk '{print $1}')
 
 if [ `git -C /home/pi/pyro-engine pull origin master | grep -c "up to date."` -ne 1 ];
     then
         echo "pyro-engine updated from github";
         cd /home/pi/pyro-engine/runner/;
-        docker kill CID;
+        docker kill $CID;
         echo "rebuild docker";
         docker-compose up -d --build;
     else
