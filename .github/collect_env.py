3fb6bc2ac745679d74a3a57cccf727f13c2ec760
frgfm <26927750+frgfm@users.noreply.github.com>
2022-08-03 19:22:13 +0200

    ci: Updated collect_env

--- .github/collect_env.py
+++ .github/collect_env.py
@@ -21,23 +21,16 @@ from collections import namedtuple
 try:
     import pyroengine
 
-    PYROVISION_AVAILABLE = True
+    ENGINE_AVAILABLE = True
 except (ImportError, NameError, AttributeError):
-    PYROVISION_AVAILABLE = False
+    ENGINE_AVAILABLE = False
 
 try:
-    import torch
+    import onnxruntime
 
-    TORCH_AVAILABLE = True
+    ONNX_AVAILABLE = True
 except (ImportError, NameError, AttributeError):
-    TORCH_AVAILABLE = False
-
-try:
-    import torchvision
-
-    VISION_AVAILABLE = True
-except (ImportError, NameError, AttributeError):
-    VISION_AVAILABLE = False
+    ONNX_AVAILABLE = False
 
 PY3 = sys.version_info >= (3, 0)
 
@@ -47,15 +40,9 @@ SystemEnv = namedtuple(
     "SystemEnv",
     [
         "pyroengine_version",
-        "torch_version",
-        "torchvision_version",
+        "onnxruntime_version",
         "os",
         "python_version",
-        "is_cuda_available",
-        "cuda_runtime_version",
-        "nvidia_driver_version",
-        "nvidia_gpu_models",
-        "cudnn_version",
     ],
 )
 
@@ -89,74 +76,6 @@ def run_and_parse_first_match(run_lambda, command, regex):
     if match is None:
         return None
     return match.group(1)
-
-
-def get_nvidia_driver_version(run_lambda):
-    if get_platform() == "darwin":
-        cmd = "kextstat | grep -i cuda"
-        return run_and_parse_first_match(run_lambda, cmd, r"com[.]nvidia[.]CUDA [(](.*?)[)]")
-    smi = get_nvidia_smi()
-    return run_and_parse_first_match(run_lambda, smi, r"Driver Version: (.*?) ")
-
-
-def get_gpu_info(run_lambda):
-    if get_platform() == "darwin":
-        if TORCH_AVAILABLE and torch.cuda.is_available():
-            return torch.cuda.get_device_name(None)
-        return None
-    smi = get_nvidia_smi()
-    uuid_regex = re.compile(r" \(UUID: .+?\)")
-    rc, out, _ = run_lambda(smi + " -L")
-    if rc != 0:
-        return None
-    # Anonymize GPUs by removing their UUID
-    return re.sub(uuid_regex, "", out)
-
-
-def get_running_cuda_version(run_lambda):
-    return run_and_parse_first_match(run_lambda, "nvcc --version", r"release .+ V(.*)")
-
-
-def get_cudnn_version(run_lambda):
-    """This will return a list of libcudnn.so; it's hard to tell which one is being used"""
-    if get_platform() == "win32":
-        cudnn_cmd = 'where /R "%CUDA_PATH%\\bin" cudnn*.dll'
-    elif get_platform() == "darwin":
-        # CUDA libraries and drivers can be found in /usr/local/cuda/. See
-        # https://docs.nvidia.com/cuda/cuda-installation-guide-mac-os-x/index.html#install
-        # https://docs.nvidia.com/deeplearning/sdk/cudnn-install/index.html#installmac
-        # Use CUDNN_LIBRARY when cudnn library is installed elsewhere.
-        cudnn_cmd = "ls /usr/local/cuda/lib/libcudnn*"
-    else:
-        cudnn_cmd = 'ldconfig -p | grep libcudnn | rev | cut -d" " -f1 | rev'
-    rc, out, _ = run_lambda(cudnn_cmd)
-    # find will return 1 if there are permission errors or if not found
-    if len(out) == 0 or (rc != 1 and rc != 0):
-        lib = os.environ.get("CUDNN_LIBRARY")
-        if lib is not None and os.path.isfile(lib):
-            return os.path.realpath(l)
-        return None
-    files = set()
-    for fn in out.split("\n"):
-        fn = os.path.realpath(fn)  # eliminate symbolic links
-        if os.path.isfile(fn):
-            files.add(fn)
-    if not files:
-        return None
-    # Alphabetize the result because the order is non-deterministic otherwise
-    files = list(sorted(files))
-    if len(files) == 1:
-        return files[0]
-    result = "\n".join(files)
-    return "Probably one of the following:\n{}".format(result)
-
-
-def get_nvidia_smi():
-    # Note: nvidia-smi is currently available only on Windows and Linux
-    smi = "nvidia-smi"
-    if get_platform() == "win32":
-        smi = '"C:\\Program Files\\NVIDIA Corporation\\NVSMI\\%s"' % smi
-    return smi
 
 
 def get_platform():
@@ -220,49 +139,31 @@ def get_os(run_lambda):
 def get_env_info():
     run_lambda = run
 
-    if pyroengine_AVAILABLE:
+    if ENGINE_AVAILABLE:
         pyroengine_str = pyroengine.__version__
     else:
         pyroengine_str = "N/A"
 
-    if TORCH_AVAILABLE:
-        torch_str = torch.__version__
-        cuda_available_str = torch.cuda.is_available()
+    if ONNX_AVAILABLE:
+        onnxruntime_str = onnxruntime.__version__
     else:
-        torch_str = cuda_available_str = "N/A"
-
-    if VISION_AVAILABLE:
-        torchvision_str = torchvision.__version__
-    else:
-        torchvision_str = "N/A"
+        onnxruntime_str = "N/A"
 
     return SystemEnv(
         pyroengine_version=pyroengine_str,
-        torch_version=torch_str,
-        torchvision_version=torchvision_str,
-        python_version="{}.{}".format(sys.version_info[0], sys.version_info[1]),
-        is_cuda_available=cuda_available_str,
-        cuda_runtime_version=get_running_cuda_version(run_lambda),
-        nvidia_gpu_models=get_gpu_info(run_lambda),
-        nvidia_driver_version=get_nvidia_driver_version(run_lambda),
-        cudnn_version=get_cudnn_version(run_lambda),
+        onnxruntime_version=onnxruntime_str,
+        python_version=".".join(map(str, sys.version_info[:3])),
         os=get_os(run_lambda),
     )
 
 
 env_info_fmt = """
-pyroengine version: {pyroengine_version}
-PyTorch version: {torch_version}
-Torchvision version: {torchvision_version}
+PyroEngine version: {pyroengine_version}
+ONNX runtime version: {onnxruntime_version}
 
 OS: {os}
 
 Python version: {python_version}
-Is CUDA available: {is_cuda_available}
-CUDA runtime version: {cuda_runtime_version}
-GPU models and configuration: {nvidia_gpu_models}
-Nvidia driver version: {nvidia_driver_version}
-cuDNN version: {cudnn_version}
 """.strip()
 
 
@@ -289,21 +190,6 @@ def pretty_str(envinfo):
         return string
 
     mutable_dict = envinfo._asdict()
-
-    # If nvidia_gpu_models is multiline, start on the next line
-    mutable_dict["nvidia_gpu_models"] = maybe_start_on_next_line(envinfo.nvidia_gpu_models)
-
-    # If the machine doesn't have CUDA, report some fields as 'No CUDA'
-    dynamic_cuda_fields = [
-        "cuda_runtime_version",
-        "nvidia_gpu_models",
-        "nvidia_driver_version",
-    ]
-    all_cuda_fields = dynamic_cuda_fields + ["cudnn_version"]
-    all_dynamic_cuda_fields_missing = all(mutable_dict[field] is None for field in dynamic_cuda_fields)
-    if TORCH_AVAILABLE and not torch.cuda.is_available() and all_dynamic_cuda_fields_missing:
-        for field in all_cuda_fields:
-            mutable_dict[field] = "No CUDA"
 
     # Replace True with Yes, False with No
     mutable_dict = replace_bools(mutable_dict)
