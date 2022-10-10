278181b220ccc7f6161426531713f29260b91237
mateo <mateo.lostanlen@gmail.com>
2022-10-10 14:40:44 +0200

    fix and improove update scrip

--- scripts/update_script.sh
+++ scripts/update_script.sh
@@ -3,21 +3,17 @@
 # pull origin main
 #- if any change:
 #    kill container
-#	 rebuild docker compose
+#    rebuild docker compose
 # 
 # This script must be run with a crontab, run every day at 3am
 # 0 3 * * * bash /home/pi/pyro-engine/scripts/update_script.sh
 
 
-CID=$(docker ps | grep runner_pyro-engine_1 | awk '{print $1}')
-
 if [ `git -C /home/pi/pyro-engine pull origin main | grep -c "up to date."` -ne 1 ];
     then
-        echo "pyro-engine updated from github";
-        cd /home/pi/pyro-engine/runner/;
-        docker kill $CID;
-        echo "rebuild docker";
-        docker-compose up -d --build;
-    else
         echo "pyro-engine up to date";
+    else
+        echo "pyro-engine updated from github";
+        make -C /home/pi/pyro-engine stop
+        make -C /home/pi/pyro-engine run
 fi;
