139684fb239608636fc083452a67505ab5193c43
mateo <mateo.lostanlen@gmail.com>
2022-11-23 23:23:39 +0100

    adapt tests

--- tests/test_vision.py
+++ tests/test_vision.py
@@ -4,21 +4,45 @@ from huggingface_hub import hf_hub_download
 from pyroengine.vision import Classifier
 
 
-def test_classifier(mock_wildfire_image):
+def test_classifier(mock_wildfire_image, tmpdir_factory):
+
+    # Cache
+    folder = str(tmpdir_factory.mktemp("vision_cache"))
 
     # Instantiate the ONNX model
-    model = Classifier("pyronear/rexnet1_0x", revision="983c33f0b1aacde134e934e91550e6bd8651b31c")
+    model = Classifier(
+        [
+            "pyronear/rexnet1_0x",
+        ],
+        folder,
+    )
     # Check preprocessing
-    out = model.preprocess_image(mock_wildfire_image)
+    out = model.preprocess_image(0, mock_wildfire_image)
     assert isinstance(out, np.ndarray) and out.dtype == np.float32
     assert out.shape == (1, 3, 256, 384)
     # Check inference
     out = model(mock_wildfire_image)
-    assert isinstance(out, np.ndarray) and out.dtype == np.float32
-    assert out.shape == (1,)
+    assert isinstance(out, np.float32) and out.dtype == np.float32
     assert out >= 0 and out <= 1
 
-    # Load static file
-    cfg_path = hf_hub_download("pyronear/rexnet1_0x", filename="config.json")
-    model_path = hf_hub_download("pyronear/rexnet1_0x", filename="model.onnx")
-    Classifier("pyronear/rexnet1_0x", cfg_path=cfg_path, model_path=model_path)
+    # Test model ensemble
+
+    # Instantiate the ONNX model
+    model = Classifier(
+        [
+            "pyronear/rexnet1_0x",
+            "pyronear/rexnet1_3x",
+        ],
+        folder,
+    )
+    # Check preprocessing
+    out = model.preprocess_image(0, mock_wildfire_image)
+    assert isinstance(out, np.ndarray) and out.dtype == np.float32
+    assert out.shape == (1, 3, 256, 384)
+    out = model.preprocess_image(1, mock_wildfire_image)
+    assert isinstance(out, np.ndarray) and out.dtype == np.float32
+    assert out.shape == (1, 3, 256, 384)
+    # Check inference
+    out = model(mock_wildfire_image)
+    assert isinstance(out, np.float32) and out.dtype == np.float32
+    assert out >= 0 and out <= 1
