0b6df7481a6128f9b7595fff59d3c43bb136f87a
mateo <mateo.lostanlen@gmail.com>
2023-01-29 19:38:44 +0100

    update output file path

--- scripts/get_sunset_sunrise.sh
+++ scripts/get_sunset_sunrise.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# This script must be run with a crontab, run every day at 4am
+# 0 4 * * * bash /home/pi/pyro-engine/scripts/update_script.sh
 
 # First obtain a location code from: https://weather.codes/search/
-
 # Insert your location. For example FRXX0076 is a location code for Paris, FRANCE
+
 location="FRXX0076"
 tmpfile=/tmp/$location.out
 
@@ -16,6 +18,6 @@ SUNS=$(grep SunriseSunset "$tmpfile" | grep -oE '((1[0-2]|0?[1-9]):([0-5][0-9]) 
 sunrise=$(date --date="$SUNR" +%R)
 sunset=$(date --date="$SUNS" +%R)
 
-echo $sunrise > sunset_sunrise.txt
-echo $sunset >> sunset_sunrise.txt
+echo $sunrise > /home/pi/pyro-engine/data/sunset_sunrise.txt
+echo $sunset >> /home/pi/pyro-engine/data/sunset_sunrise.txt
 
