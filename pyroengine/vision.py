889f67e2d77938f706ba430dfad729c0756ce513
mateo <mateo.lostanlen@gmail.com>
2023-01-30 00:20:02 +0100

    unused import

--- pyroengine/vision.py
+++ pyroengine/vision.py
@@ -3,10 +3,9 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
 
-import json
 import os
 import urllib
-from typing import Any, Optional
+from typing import Optional
 
 import numpy as np
 import onnxruntime
