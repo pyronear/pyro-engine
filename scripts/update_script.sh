35135b5ecc9daa1b6fb3ae413449bce7afb67ea8
Mateo <mateo.lostanlen@gmail.com>
2024-04-09 10:02:41 +0200

    new script (#178)
    * new script
    
    * wrong file name

--- scripts/update_script.sh
+++ scripts/update_script.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 # This script performs:
-# pull origin main
+# fetch origin main
 #- if any change:
-#    kill container
-#    rebuild docker compose
+#    pull changes
+#    any others change needed
 # 
-# This script must be run with a crontab, run every day at 3am
-# 0 3 * * * bash /home/pi/pyro-engine/scripts/update_script.sh
+# This script must be run with a crontab, run every hour
+# 0 * * * * bash /home/pi/pyro-engine/scripts/update_script.sh >> /home/pi/pyro-engine/logfile.log 2>&1
 
 
-if [ `git -C /home/pi/pyro-engine pull origin main | grep -c "up to date."` -ne 1 ];
-    then
-        echo "pyro-engine up to date";
-    else
-        echo "pyro-engine updated from github";
-        make -C /home/pi/pyro-engine stop
-        make -C /home/pi/pyro-engine run
-fi;
+# Print current date and time
+echo "$(date): Checking for updates"
+
+# Navigate to the repository directory
+cd /home/pi/pyro-engine
+
+# Check for updates and pull
+git fetch origin
+HEADHASH=$(git rev-parse HEAD)
+UPSTREAMHASH=$(git rev-parse origin/main)
+
+if [ "$HEADHASH" != "$UPSTREAMHASH" ]
+then
+    echo "$(date): New changes detected ! Updating and executing script..."
+    git pull origin main
+    # Add any action here
+    echo "$(date): Update done !"
+
+else
+    echo "$(date): No changes detected"
+fi
+
