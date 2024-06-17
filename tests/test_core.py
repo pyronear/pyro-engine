cc60dc98eff582d1b30de247d06d42aabd1d75bd
MateoLostanlen <mateo.lostanlen@gmail.com>
2024-06-17 11:02:48 +0200

    update test

--- tests/test_core.py
+++ tests/test_core.py
@@ -78,10 +78,8 @@ def test_capture_images(system_controller):
     for camera in system_controller.cameras:
         capture_camera_image((camera, queue))
 
-    print(f"Queue size after capture: {queue.qsize()}")
     assert queue.qsize() == 1
     cam_id, frame = queue.get(timeout=1)  # Use timeout to wait for the item
-    print(f"Camera ID: {cam_id}, Frame size: {frame.size}")
     assert cam_id == "192.168.1.1"
     assert isinstance(frame, Image.Image)
 
@@ -97,10 +95,8 @@ def test_capture_images_ptz(system_controller_ptz):
         time.sleep(0.1)
         retries -= 1
 
-    print(f"Queue size after capture: {queue.qsize()}")
     assert queue.qsize() == 2
     cam_id, frame = queue.get(timeout=1)  # Use timeout to wait for the item
-    print(f"Camera ID: {cam_id}, Frame size: {frame.size}")
     assert cam_id == "192.168.1.1_1"
     assert isinstance(frame, Image.Image)
 
@@ -155,16 +151,16 @@ def test_run_capture_exception(system_controller):
             pass
 
 
-# def test_capture_camera_image_exception():
-#     queue = Queue(maxsize=10)
-#     camera = MagicMock()
-#     camera.cam_type = "static"
-#     camera.ip_address = "192.168.1.1"
-#     camera.capture.side_effect = Exception("Capture error")
+def test_capture_camera_image_exception():
+    queue = Queue(maxsize=10)
+    camera = MagicMock()
+    camera.cam_type = "static"
+    camera.ip_address = "192.168.1.1"
+    camera.capture.side_effect = Exception("Capture error")
 
-#     capture_camera_image((camera, queue))
+    capture_camera_image((camera, queue))
 
-#     assert queue.empty()
+    assert queue.qsize() == 0
 
 
 def test_repr_method(system_controller):
@@ -189,10 +185,8 @@ def test_capture_camera_image():
 
     capture_camera_image((camera, queue))
 
-    print(f"Queue size after capture: {queue.qsize()}")
-    assert not queue.empty()
+    assert queue.qsize() == 1
     cam_id, frame = queue.get(timeout=1)  # Use timeout to wait for the item
-    print(f"Camera ID: {cam_id}, Frame size: {frame.size}")
     assert cam_id == "192.168.1.1"
     assert isinstance(frame, Image.Image)
 
@@ -213,9 +207,7 @@ def test_capture_camera_image_ptz():
         time.sleep(0.1)
         retries -= 1
 
-    print(f"Queue size after capture: {queue.qsize()}")
     assert queue.qsize() == 2
     cam_id, frame = queue.get(timeout=1)  # Use timeout to wait for the item
-    print(f"Camera ID: {cam_id}, Frame size: {frame.size}")
     assert cam_id == "192.168.1.1_1"
     assert isinstance(frame, Image.Image)
