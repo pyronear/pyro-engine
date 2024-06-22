409f86a4e3300c6c6bd724d9a11823b9fd186302
MateoLostanlen <mateo.lostanlen@gmail.com>
2024-06-22 17:11:43 +0200

    put back letterbox padding

--- pyroengine/vision.py
+++ pyroengine/vision.py
@@ -14,7 +14,7 @@ import onnxruntime
 from huggingface_hub import HfApi  # type: ignore[import-untyped]
 from PIL import Image
 
-from .utils import DownloadProgressBar, nms, xywh2xyxy
+from .utils import DownloadProgressBar, letterbox, nms, xywh2xyxy
 
 __all__ = ["Classifier"]
 
@@ -41,7 +41,7 @@ class Classifier:
         model_path: model path
     """
 
-    def __init__(self, model_path: Optional[str] = "data/model.onnx", base_img_size: int = 640) -> None:
+    def __init__(self, model_path: Optional[str] = "data/model.onnx", img_size: tuple = (640, 640)) -> None:
         if model_path is None:
             model_path = "data/model.onnx"
 
@@ -67,7 +67,7 @@ class Classifier:
             self.download_model(model_path, expected_sha256)
 
         self.ort_session = onnxruntime.InferenceSession(model_path)
-        self.base_img_size = base_img_size
+        self.img_size = img_size
 
     def get_sha(self, siblings):
         # Extract the SHA256 hash from the model files metadata
@@ -99,7 +99,7 @@ class Classifier:
                 return json.load(f)
         return None
 
-    def preprocess_image(self, pil_img: Image.Image, new_img_size: list) -> Tuple[np.ndarray, Tuple[int, int]]:
+    def preprocess_image(self, pil_img: Image.Image) -> Tuple[np.ndarray, Tuple[int, int]]:
         """Preprocess an image for inference
 
         Args:
@@ -111,20 +111,15 @@ class Classifier:
             - Padding information as a tuple of integers (pad_height, pad_width).
         """
 
-        np_img = cv2.resize(np.array(pil_img), new_img_size, interpolation=cv2.INTER_LINEAR)
+        np_img, pad = letterbox(np.array(pil_img), self.img_size)  # Applies letterbox resize with padding
         np_img = np.expand_dims(np_img.astype("float"), axis=0)  # Add batch dimension
         np_img = np.ascontiguousarray(np_img.transpose((0, 3, 1, 2)))  # Convert from BHWC to BCHW format
         np_img = np_img.astype("float32") / 255  # Normalize to [0, 1]
 
-        return np_img
+        return np_img, pad
 
     def __call__(self, pil_img: Image.Image, occlusion_mask: Optional[np.ndarray] = None) -> np.ndarray:
-
-        w, h = pil_img.size
-        ratio = self.base_img_size / max(w, h)
-        new_img_size = [int(ratio * w), int(ratio * h)]
-        new_img_size = [x - x % 32 for x in new_img_size]  # size need to be a multiple of 32 to fit the model
-        np_img = self.preprocess_image(pil_img, new_img_size)
+        np_img, pad = self.preprocess_image(pil_img)
 
         # ONNX inference
         y = self.ort_session.run(["output0"], {"images": np_img})[0][0]
@@ -139,9 +134,12 @@ class Classifier:
 
         # Normalize preds
         if len(y) > 0:
-            # Normalize Output
-            y[:, :4:2] /= new_img_size[0]
-            y[:, 1:4:2] /= new_img_size[1]
+            # Remove padding
+            left_pad, top_pad = pad
+            y[:, :4:2] -= left_pad
+            y[:, 1:4:2] -= top_pad
+            y[:, :4:2] /= self.img_size[1] - 2 * left_pad
+            y[:, 1:4:2] /= self.img_size[0] - 2 * top_pad
         else:
             y = np.zeros((0, 5))  # normalize output
 
