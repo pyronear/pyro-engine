00dbbcf7c1029d7d4a938db1b246c15e1cf5fee9
MateoLostanlen <mateo.lostanlen@gmail.com>
2024-07-09 13:57:50 +0200

    fix test vision

--- tests/test_vision.py
+++ tests/test_vision.py
@@ -7,102 +7,29 @@ import pytest
 from pyroengine.vision import Classifier
 
 METADATA_PATH = "data/model_metadata.json"
-model_path = "data/model.onnx"
+model_path = "data/yolov8s.onnx"
 sha = "9f1b1c2654d98bbed91e514ce20ea73a0a5fbd1111880f230d516ed40ea2dc58"
-
-
-def custom_isfile_false(path):
-    if path == model_path:
-        return False  # or True based on your test case
-    return True  # Default behavior for other paths
-
-
-def custom_isfile_true(path):
-    if path == model_path:
-        return True  # or True based on your test case
-    return True  # Default behavior for other paths
 
 
 # Test for the case : the model doesn't exist
 def test_classifier(mock_wildfire_image):
     print("test_classifier")
-    with patch("os.path.isfile", side_effect=custom_isfile_false):
-        # Instantiate the ONNX model
-        model = Classifier()
-        # Check preprocessing
-        out, pad = model.preprocess_image(mock_wildfire_image)
-        assert isinstance(out, np.ndarray) and out.dtype == np.float32
-        assert out.shape == (1, 3, 640, 640)
-        assert isinstance(pad, tuple)
-        # Check inference
-        out = model(mock_wildfire_image)
-        assert out.shape == (1, 5)
-        conf = np.max(out[:, 4])
-        assert conf >= 0 and conf <= 1
 
-        # Test mask
-        mask = np.ones((384, 640))
-        out = model(mock_wildfire_image, mask)
-        print(out)
-        assert out.shape == (1, 5)
+    # Instantiate the ONNX model
+    model = Classifier(model_folder="data")
+    # Check inference
+    out = model(mock_wildfire_image)
+    assert out.shape[1] == 5
+    conf = np.max(out[:, 4])
+    assert 0 <= conf <= 1
 
-        mask = np.zeros((384, 640))
-        out = model(mock_wildfire_image, mask)
-        print(out)
-        assert out.shape == (0, 5)
-        os.remove(model_path)
-        os.remove(METADATA_PATH)
+    model = Classifier(model_folder="data", format="onnx")
 
+    # Test mask
+    mask = np.ones((384, 640))
+    out = model(mock_wildfire_image, mask)
+    assert out.shape == (1, 5)
 
-# Test that the model is not loaded
-def test_no_download():
-    print("test_no_download")
-    data = {"sha256": sha}
-    with patch("os.path.isfile", side_effect=custom_isfile_true):
-        with patch("pyroengine.vision.Classifier.load_metadata", return_value=data):
-            with patch("onnxruntime.InferenceSession", return_value=None):
-                Classifier()
-    assert os.path.isfile(model_path) is False
-
-
-# Test if sha are not the same
-@patch("pyroengine.vision.urlretrieve")
-@patch("pyroengine.vision.DownloadProgressBar")
-def test_sha_inequality(mock_download_progress, mock_urlretrieve):
-    print("test_sha_inequality")
-    data = {"sha256": "falsesha"}
-
-    # Mock urlretrieve to create a fake file
-    def fake_urlretrieve(url, filename, reporthook=None):
-        with open(filename, "w") as f:
-            f.write("fake model content")
-
-    mock_urlretrieve.side_effect = fake_urlretrieve
-    # Mock the DownloadProgressBar context manager
-    mock_progress_bar_instance = MagicMock()
-    mock_download_progress.return_value.__enter__.return_value = mock_progress_bar_instance
-
-    with patch("os.path.isfile", side_effect=custom_isfile_true):
-        with patch("pyroengine.vision.Classifier.load_metadata", return_value=data):
-            with patch(
-                "pyroengine.vision.Classifier.get_sha",
-                return_value=sha,
-            ):
-                with patch("onnxruntime.InferenceSession", return_value=None):
-                    with patch("os.remove", return_value=True):
-                        model = Classifier()
-
-    assert os.path.isfile(model_path) is True
-    assert model.load_metadata("non_existent_metadata.json") is None
-    os.remove(model_path)
-    os.remove(METADATA_PATH)
-
-
-# Test for raising ValueError if expected_sha256 is not found
-def test_raise_value_error_if_no_sha256():
-    print("test_raise_value_error_if_no_sha256")
-    with patch("pyroengine.vision.Classifier.get_sha", return_value=""):
-        with pytest.raises(
-            ValueError, match="SHA256 hash for the model file not found in the Hugging Face model metadata."
-        ):
-            Classifier(model_path="non_existent_model.onnx")
+    mask = np.zeros((384, 640))
+    out = model(mock_wildfire_image, mask)
+    assert out.shape == (0, 5)
