26cfbe33e3d10f3a3e5764baf85dfa6e9ac9ca1a
Ronan <ronan_2@hotmail.com>
2024-09-19 11:04:35 +0200

    fix permission error when used locally

--- pyroengine/logger_config.py
+++ pyroengine/logger_config.py
@@ -4,19 +4,13 @@ from logging.handlers import TimedRotatingFileHandler
 
 # Define the logging format
 log_format = "%(asctime)s | %(levelname)s: %(message)s"
-os.makedirs(os.path.dirname("/var/log/engine.log"), exist_ok=True)
-
-if not os.path.exists("/var/log/engine.log"):
-    open("/var/log/engine.log", "a").close()  # Create the file if it doesn't exist
-
-os.chmod("/var/log/engine.log", 0o664)
 
 # Create a StreamHandler (for stdout)
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(logging.Formatter(log_format))
 
 # Create a TimedRotatingFileHandler (for writing logs to a new file every day)
-file_handler = TimedRotatingFileHandler("/var/log/engine.log", when="midnight", interval=1, backupCount=5)
+file_handler = TimedRotatingFileHandler("engine.log", when="midnight", interval=1, backupCount=5)
 file_handler.setFormatter(logging.Formatter(log_format))
 file_handler.suffix = "%Y-%m-%d"  # Adds a date suffix to the log file name
 
