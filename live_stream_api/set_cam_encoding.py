95e5d14c9beaa4da489150d4581b8f6e8d85a9b4
MateoLOSTANLEN <mateo.lostanlen@gmail.com>
2025-04-28 09:39:52 +0200

    fix name

--- live_stream_api/set_cam_encoding.py
+++ live_stream_api/set_cam_encoding.py
@@ -12,6 +12,7 @@ load_dotenv()
 CAM_USER = os.getenv("CAM_USER")
 CAM_PWD = os.getenv("CAM_PWD")
 
+
 # ----------------------- NEW ENCODING SETTINGS -----------------------
 
 # Main Stream possible values:
@@ -42,7 +43,7 @@ def get_token(camera_ip):
     payload = [
         {
             "cmd": "Login",
-            "param": {"User": {"Version": "0", "CAM_USER": CAM_USER, "CAM_PWD": CAM_PWD}},
+            "param": {"User": {"Version": "0", "userName": CAM_USER, "password": CAM_USER}},
         }
     ]
     headers = {"Content-Type": "application/json"}
@@ -142,8 +143,8 @@ if __name__ == "__main__":
 
     camera_ip = args.ip
 
-    if not CAM_USER or not CAM_PWD:
-        print("❌ CAM_USER or CAM_PWD not found. Please set them in a .env file.")
+    if not CAM_USER or not CAM_USER:
+        print("❌ USERNAME or CAM_USER not found. Please set them in a .env file.")
         exit(1)
 
     token = get_token(camera_ip)
