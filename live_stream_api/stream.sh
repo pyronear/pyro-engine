9cdb263e9ea98c3c3c930365e217712156d6f1a4
MateoLOSTANLEN <mateo.lostanlen@gmail.com>
2025-04-28 08:57:26 +0200

    adapt exemple

--- live_stream_api/stream.sh
+++ live_stream_api/stream.sh
@@ -1,4 +1,4 @@
-ffmpeg -fflags discardcorrupt+nobuffer -flags low_delay -rtsp_transport tcp \
--i rtsp://admin:@Pyronear@169.254.40.1:554/h264Preview_01_sub \
--c:v libx264 -bf 0 -b:v 300k -r 10 -preset veryfast -tune zerolatency -an -f mpegts \
-'srt://91.134.47.14:8890?pkt_size=1316&mode=caller&latency=500&streamid=publish:mateostream'
+ffmpeg -hide_banner -loglevel warning -fflags discardcorrupt+nobuffer -flags low_delay -rtsp_transport tcp \
+-i rtsp://admin:@Pyronear@192.168.1.11:554/h264Preview_01_sub \
+-c:v libx264 -bf 0 -g 5 -b:v 500k -r 10 -preset veryfast -tune zerolatency -flush_packets 1 -an -f mpegts \
+"srt://91.134.47.14:8890?pkt_size=1316&mode=caller&latency=30&streamid=publish:mateostream"
