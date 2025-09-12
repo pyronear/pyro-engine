a01182695a95c1a105a53fa64944412ca21bdaea
MateoLOSTANLEN <mateo.lostanlen@gmail.com>
2025-09-12 10:40:40 +0200

    missing headers

--- reolink_api/src/camera/config.py
+++ reolink_api/src/camera/config.py
@@ -1,4 +1,7 @@
-# Copyright (C) 2020-2025, Pyronear.
+# Copyright (C) 2022-2025, Pyronear.
+
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
 
 from __future__ import annotations
 
