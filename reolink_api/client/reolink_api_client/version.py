0526b604e6b8348efb3762e9a7baa1b65a43a727
Mateo <mateo.lostanlen@gmail.com>
2025-11-16 17:27:22 +0100

    delete old api

--- reolink_api/client/reolink_api_client/version.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2020-2025, Pyronear.
-
-# This program is licensed under the Apache License 2.0.
-# See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
-
-
-__version__ = "0.1.0.dev0"
