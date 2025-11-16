30f91428939fe67691a49ae8aada0214dbc58be3
Mateo <mateo.lostanlen@gmail.com>
2025-11-16 12:07:30 +0100

    add inits

---
+++ pyro_camera_api/pyro_camera_api/camera/backends/__init__.py
@@ -0,0 +1,7 @@
+# pyro_camera_api/camera/backends/__init__.py
+from .reolink import ReolinkCamera
+from .rtsp import RTSPCamera
+from .url import URLCamera
+
+__all__ = ["ReolinkCamera", "RTSPCamera", "URLCamera"]
+
--- pyro_camera_api/pyro_camera_api/core/__init__.py
+++ pyro_camera_api/pyro_camera_api/core/__init__.py
@@ -13,3 +13,17 @@ from .config import (
     SRT_STREAMID_PREFIX,
     FFMPEG_PARAMS,
 )
+
+__all__ = [
+    "CAM_USER",
+    "CAM_PWD",
+    "MEDIAMTX_SERVER_IP",
+    "RAW_CONFIG",
+    "STREAMS",
+    "SRT_PKT_SIZE",
+    "SRT_MODE",
+    "SRT_LATENCY",
+    "SRT_PORT_START",
+    "SRT_STREAMID_PREFIX",
+    "FFMPEG_PARAMS",
+]
