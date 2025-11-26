733b1f52305b6268871b5a530f4b3edb61bbe266
Mateo <mateo.lostanlen@gmail.com>
2025-11-26 09:17:30 +0100

    fix header

--- pyro_camera_api/pyro_camera_api/camera/backends/mock.py
+++ pyro_camera_api/pyro_camera_api/camera/backends/mock.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2022-2025, Pyronear.
-#
+
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
+
 
 from __future__ import annotations
 
