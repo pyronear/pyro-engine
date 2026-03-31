7f9e3f114750be24af9d07dd9bc9a85c5700809a
Mateo <mateo.lostanlen@gmail.com>
2026-03-31 18:15:53 +0200

    fix autodocs deps

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,8 @@ extensions = [
 
 napoleon_use_ivar = True
 
+autodoc_mock_imports = ["ncnn", "onnxruntime"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
