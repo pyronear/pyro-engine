d0db7b56f504bcc2dc7590d8e227d9c1a2872573
Mateo <mateo.lostanlen@gmail.com>
2026-04-07 16:00:44 +0200

    update test pose id

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -145,7 +145,7 @@ def test_engine_online(tmpdir_factory, mock_wildfire_stream, mock_wildfire_image
     # With API
     load_dotenv(Path(__file__).parent.parent.joinpath(".env").absolute())
     api_url = os.environ.get("API_URL")
-    cam_creds = {"dummy_cam": (os.environ.get("API_TOKEN"), 62, None)}
+    cam_creds = {"dummy_cam": (os.environ.get("API_TOKEN"), 66, None)}
     # Skip the API-related tests if the URL is not specified
 
     if isinstance(api_url, str):
